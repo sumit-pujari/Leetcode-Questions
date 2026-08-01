4d4afd5710841617a26554fbfa66864641aaba63
Sumit Pujari <sumitpujari1012@gmail.com>
2026-08-01 22:15:19 +0530

    Time: 95 ms (19.36%) | Memory: 9.7 MB (56.39%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+int solve(vector<int>& nums,int i,int j){
+    if(i==j) return nums[i];
+    
+    int left=nums[i]-solve(nums,i+1,j);
+    int right=nums[j]-solve(nums,i,j-1);
+    
+    return max(left,right);
+    
+}
+    bool predictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        int ans= solve(nums,0,n-1);
+        if(ans>=0) return true;
+        else return false;
+    }
+};
